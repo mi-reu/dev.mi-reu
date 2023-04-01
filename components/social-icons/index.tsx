aa5136622b738a32f2401c5ef15d49737f6259d4
mi-reu <dev.mireu@gmail.com>
2023-04-01 23:07:36 +0900

    불필요한 파일 삭제 및 이미지 위치 수정

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -1,19 +1,13 @@
-import Mail from './mail.svg'
-import Github from './github.svg'
-import Facebook from './facebook.svg'
-import Youtube from './youtube.svg'
-import Linkedin from './linkedin.svg'
-import Twitter from './twitter.svg'
+import Mail from './svgs/mail.svg'
+import Github from './svgs/github.svg'
+import Linkedin from './svgs/linkedin.svg'
 
 // Icons taken from: https://simpleicons.org/
 
 const components = {
   mail: Mail,
   github: Github,
-  facebook: Facebook,
-  youtube: Youtube,
   linkedin: Linkedin,
-  twitter: Twitter,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
