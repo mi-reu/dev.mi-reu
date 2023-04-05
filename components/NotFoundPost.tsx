b67c5286f5e152cd9282071e6854efe0eec60aee
mi-reu <dev.mireu@gmail.com>
2023-04-05 22:25:03 +0900

    404 페이지 및 NotFoundPost 수정

--- components/NotFoundPost.tsx
+++ components/NotFoundPost.tsx
@@ -3,7 +3,7 @@ const NotFoundPost = () => {
     <div className="mt-5" role="alert">
       <div className="rounded-t bg-red-500 px-4 py-2 font-bold text-white">404</div>
       <div className="rounded-b border border-t-0 border-red-400 bg-red-100 px-4 py-3 text-red-700">
-        <p>Not found post</p>
+        <p>등록된 게시글이 없습니다.</p>
       </div>
     </div>
   )
--- pages/404.tsx
+++ pages/404.tsx
@@ -9,13 +9,11 @@ export default function FourZeroFour() {
         </h1>
       </div>
       <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
-        </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">죄송합니다.</p>
+        <p className="mb-8">등록된 게시글이 없습니다.</p>
         <Link href="/">
-          <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-            Back to homepage
+          <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-rose-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-rose-700 focus:outline-none dark:hover:bg-rose-500">
+            돌아가기
           </button>
         </Link>
       </div>
