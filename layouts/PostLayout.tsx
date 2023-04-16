0527b129dee41f8bae24f5d3fc725c9b390bdc9e
mi-reu <dev.mireu@gmail.com>
2023-04-16 12:08:19 +0900

    Image 컴포넌트 css 속성 추가

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -72,7 +72,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                           width="38px"
                           height="38px"
                           alt="avatar"
-                          className="h-10 w-10 rounded-full"
+                          className="h-10 w-10 rounded-full object-cover"
                         />
                       )}
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
