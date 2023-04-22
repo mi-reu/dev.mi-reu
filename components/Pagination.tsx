0ea3474e0d5d3e066001c47c90e45dc5f1b98a51
mi-reu <dev.mireu@gmail.com>
2023-04-22 16:27:54 +0900

    Pagination 기능 개선

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -14,25 +14,43 @@ export default function Pagination({ totalPages, currentPage }: Props) {
       <nav className="flex justify-between">
         {!prevPage && (
           <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            이전 페이지
+            {`<---`}
+            <span className="sr-only">이전 페이지</span>
           </button>
         )}
         {prevPage && (
           <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-            <button>이전 페이지</button>
+            <button>
+              {`<---`}
+              <span className="sr-only">이전 페이지</span>
+            </button>
           </Link>
         )}
-        <span>
-          {currentPage} of {totalPages}
-        </span>
+        <div className="flex">
+          {Array(totalPages)
+            .fill(1)
+            .map((v, i) => (
+              <a
+                key={i}
+                className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                href={`/blog/page/${v + i}`}
+              >
+                {v + i}
+              </a>
+            ))}
+        </div>
         {!nextPage && (
           <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            다음 페이지
+            {`--->`}
+            <span className="sr-only">다음 페이지</span>
           </button>
         )}
         {nextPage && (
           <Link href={`/blog/page/${currentPage + 1}`}>
-            <button>다음 페이지</button>
+            <button>
+              {`--->`}
+              <span className="sr-only">다음 페이지</span>
+            </button>
           </Link>
         )}
       </nav>
