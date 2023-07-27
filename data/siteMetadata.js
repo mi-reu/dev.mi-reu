bc54f6055986ce258bddde51ec09b3eae274f030
mi-reu <dev.mireu@gmail.com>
2023-07-27 21:36:32 +0900

    giscus 기능 비활성화

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -28,15 +28,15 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
-    giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-    },
+    // provider: 'giscus', // supported providers: giscus, utterances, disqus
+    // giscusConfig: {
+    //   // Visit the link below, and follow the steps in the 'configuration' section
+    //   // https://giscus.app/
+    //   repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+    //   repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+    //   category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+    //   categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+    // },
   },
 }
 
